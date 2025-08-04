814f43e5684c69645b8f42e3cc674fc094daa961
Jcurtin <jcurtin007@gmail.com>
2025-08-04 09:50:59 +0900

    커밋 테스트 입니다

--- src/main/java/com/example/gittest/GittestApplication.java
+++ src/main/java/com/example/gittest/GittestApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GittestApplication {
 
+	//주석처리입니다
 	public static void main(String[] args) {
 		SpringApplication.run(GittestApplication.class, args);
 	}
 
+	
 }
