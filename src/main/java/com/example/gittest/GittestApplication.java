8f925d08ee77dae3e05e06768f0082304616b887
Jcurtin <jcurtin007@gmail.com>
2025-08-04 10:39:45 +0900

    test입니다

--- src/main/java/com/example/gittest/GittestApplication.java
+++ src/main/java/com/example/gittest/GittestApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GittestApplication {
 
-	//주석처리입니다
+	//주석처리입니다 !!!!!!
 	public static void main(String[] args) {
 		SpringApplication.run(GittestApplication.class, args);
 	}
